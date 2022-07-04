7390233e1c435d159de308ba5e09ab0460a94c41
Fourteen98 <emuhiydeen6@gmail.com>
2022-07-04 22:47:54 +0000

    change to modules

---
+++ js/index.js
@@ -0,0 +1,72 @@
+import AwesomeBooks from './modules/books.js';
+import LocalStorage from './modules/local_storage.js';
+import CreateUI from "./modules/create_ui.js";
+
+
+
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const form = document.getElementById('form');
+
+const localS = new LocalStorage();
+const ui = new CreateUI();
+
+function getVal(title, author, id) {
+    return new AwesomeBooks(title, author, id);
+}
+
+// important to hold length of local  storage
+let id = 0;
+
+function getIndexInLocalStorage() {
+    const books = localS.getLocalStorage();
+    // eslint-disable-next-line no-undef
+    return books.length;
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // Add new book to local storage
+    localS.setStorage(getVal(bookTitle.value, bookAuthor.value, id));
+
+    // create a new book in DOM
+    ui.addToUI(getVal(bookTitle.value, bookAuthor.value, id));
+
+    id += 1;
+});
+
+window.addEventListener('load', () => {
+    // get the book list back on DOM
+    ui.getUI(localS);
+
+    // set the id var to current index value before refresh
+    id = 1 + getIndexInLocalStorage();
+});
+
+
+// Navigation
+const addBookNav = document.getElementById('add-page-btn');
+const booksNav = document.getElementById('books-page-btn');
+const contactNav = document.getElementById('contact-page-btn');
+const addPage = document.getElementById('add-page');
+const booksPage = document.getElementById('books-page');
+const contactPage = document.getElementById('contact-page');
+
+addBookNav.addEventListener('click', () => {
+    addPage.classList.remove('none');
+    booksPage.classList.add('none');
+    contactPage.classList.add('none');
+});
+
+booksNav.addEventListener('click', () => {
+    addPage.classList.add('none');
+    booksPage.classList.remove('none');
+    contactPage.classList.add('none');
+});
+
+contactNav.addEventListener('click', () => {
+    addPage.classList.add('none');
+    booksPage.classList.add('none');
+    contactPage.classList.remove('none');
+})
---
+++ js/modules/create_ui.js
@@ -0,0 +1,67 @@
+import LocalStorage from "../modules/local_storage.js";
+
+const localS = new LocalStorage();
+
+const bookContainer = document.querySelector('.book-list');
+
+
+class CreateUI {
+    getUI(localStorage) {
+        this.books = localStorage.getLocalStorage();
+        let counter = 0;
+        while (counter < this.books.length) {
+            this.addToUI(this.books[counter]);
+            counter += 1;
+        }
+    }
+
+    addToUI(book) {
+        const list = document.createElement('div');
+        list.classList.add('single-book');
+
+        const info = document.createElement('div');
+        info.classList.add('info');
+        list.appendChild(info);
+
+        const bookID = document.createElement('h3');
+        bookID.textContent = book.id;
+        bookID.classList.add('book-id');
+        info.appendChild(bookID);
+
+        const title = document.createElement('h3');
+        title.textContent = book.title;
+        title.classList.add('title');
+        info.appendChild(title);
+
+        const by = document.createElement('h3');
+        by.textContent = 'by';
+        info.appendChild(by);
+
+        const author = document.createElement('h3');
+        author.textContent = (book.author);
+        author.classList.add('author');
+        info.appendChild(author);
+
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        removeBtn.textContent = 'Remove';
+        removeBtn.setAttribute('type', 'button');
+        list.appendChild(removeBtn);
+
+        list.addEventListener('click', (e) => {
+            this.remove(e.target);
+            localS.removeBookFromStorage(e.target.parentElement.firstChild.firstChild.textContent);
+        });
+
+        // adding book to ul
+        bookContainer.append(list);
+    }
+
+    remove(t) {
+        if (t.classList.contains('remove-btn')) {
+            t.parentElement.remove();
+        }
+    }
+}
+
+export default CreateUI;
